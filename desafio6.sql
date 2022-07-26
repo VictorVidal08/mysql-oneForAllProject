844df4008f4eabc6559c2b2c26f70283d95f3c45
VictorVidal08 <victorvidal08@hotmail.com>
2022-07-25 21:51:38 -0300

    feat: implementing query to display company earns informations

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+ROUND(MIN(s.subscription_price), 2) AS 'faturamento_minimo',
+ROUND(MAX(s.subscription_price), 2) AS 'faturamento_maximo',
+ROUND(AVG(s.subscription_price), 2) AS 'faturamento_medio',
+ROUND(SUM(s.subscription_price), 2) AS 'faturamento_medio'
+FROM SpotifyClone.Subscription AS s
+INNER JOIN SpotifyClone.Users AS u
+ON s.subscription_id = u.subscription_id;
